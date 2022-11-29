6f5c27af41c3bcc5a4d902ee1b8bb326e182677a
Giovanni Benner <giovanni.guilioti@benner.com.br>
2022-11-29 13:23:50 -0300

    finalizado exercicio2

--- exercicio2/Program.cs
+++ exercicio2/Program.cs
@@ -3,8 +3,15 @@ using exercicio2;
 
 Autor a = new Autor();
 a.Nome = "Giovanni";
+Autor b = new Autor();
+b.Nome = "Mateus";
+Autor c = new Autor();
+c.Nome = "Andre";
 
 Livro l = new Livro();
 l.autores.Add(a);
+l.autores.Add(b);
+l.autores.Add(c);
 
-Console.WriteLine(l.autores[0].Nome);
+foreach(var autor in l.autores)
+    Console.WriteLine(autor.Nome);
